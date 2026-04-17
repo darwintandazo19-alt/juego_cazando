1f1256eafab02c05da96338b2382b7e94385da3d
darwintandazo19-alt <darwintandazo19@gmail.com>
2026-04-17 17:38:11 -0500

    De la programacion en curso, Parte 2 literal literal 11 cumplido

---
+++ cazando.js
@@ -0,0 +1,8 @@
+let canvas=document.getElementById("areaJuego");
+let ctx=canvas.getContext("2d");
+
+function graficarGato() {
+    ctx.fillStyle="blue";
+    ctx.fillRect(0,0, canvas.witdth,20);
+
+}
