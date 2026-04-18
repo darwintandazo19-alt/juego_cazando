50b6296ace22270d4f086be5c15ac7a853fb3c66
darwintandazo19-alt <darwintandazo19@gmail.com>
2026-04-17 19:01:40 -0500

    se creo la comida y el gato con la ubicacion correcta en el canvas

--- cazando.js
+++ cazando.js
@@ -1,8 +1,19 @@
 let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
 
+function iniciarJuego(){
+    graficarGato();
+    graficarComida();
+
+}
 function graficarGato() {
     ctx.fillStyle="blue";
-    ctx.fillRect(0,0, canvas.witdth,20);
+    ctx.fillRect(canvas.width/4,canvas.height/3,canvas.width*20/40,canvas.height*4/20);
+
+}
+
+function graficarComida() {
+    ctx.fillStyle="blue";
+    ctx.fillRect(0,0,100,50);
 
 }
